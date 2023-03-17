57cc23a684f6ab19482d3b835d31684a4ee58192
kaloyanhrstov <116978281+kaloyanhrstov@users.noreply.github.com>
2023-03-17 22:02:07 +0200

    Update views.py

--- ecommerce_django/accounts/views.py
+++ ecommerce_django/accounts/views.py
@@ -67,37 +67,4 @@ class UserDeleteView(views.DeleteView):
     template_name = 'accounts/profile-delete-page.html'
     model = UserModel
     success_url = reverse_lazy('index')
-
-
-# @unauthenticated_user
-# def register_page(request):
-#     form = CreateUserForm
-#
-#     if request.method == 'POST':
-#         form = CreateUserForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#
-#             user = form.cleaned_data.get('username')
-#             messages.success(request, 'Account was created for ' + user)
-#             return redirect('login')
-#
-#     context = {'form': form}
-#     return render(request, 'accounts/register.html', context)
-
-# @unauthenticated_user
-# def login_page(request):
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#
-#         user = authenticate(request, username=username, password=password)
-#
-#         if user is not None:
-#             login(request, user)
-#             return redirect('store')
-#         else:
-#             messages.info(request, 'Username or Password is incorrect')
-#
-#     context = {}
-#     return render(request, 'accounts/login.html', context)
+    
